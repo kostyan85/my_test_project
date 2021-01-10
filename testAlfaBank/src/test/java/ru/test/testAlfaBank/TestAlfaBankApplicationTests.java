9c192b4ccd0795dc6e17c20ee3fe229512990e71
Konstantin K <perevoz_85@bk.ru>
2021-01-10 19:05:08 +0300

    3 commit

--- testAlfaBank/src/test/java/ru/test/testAlfaBank/TestAlfaBankApplicationTests.java
+++ testAlfaBank/src/test/java/ru/test/testAlfaBank/TestAlfaBankApplicationTests.java
@@ -2,28 +2,14 @@ package ru.test.testAlfaBank;
 
 import org.junit.jupiter.api.Test;
 
-import ru.test.testAlfaBank.service.CurrencyService;
+
 
 
 class TestAlfaBankApplicationTests {
 
     @Test
     void contextLoads() {
-//
-        CurrencyService currencyService = new CurrencyService();
-        currencyService.getYesterdayDate();
-        System.out.println();
-//        CurrencyBuilder target = Feign.builder()
-//
-//                .client(new OkHttpClient())
-//                .encoder(new GsonEncoder())
-//                .decoder(new GsonDecoder())
-//                .target(CurrencyBuilder.class, "https://api.giphy.com/v1/gifs/random?api_key=cUYFpUjEG2GR7jDNA8DR2L4kMmu7QxCM&tags=broke");
-////
-//        gifBuilder.setUrl("https://api.giphy.com/v1/gifs/random?api_key=cUYFpUjEG2GR7jDNA8DR2L4kMmu7QxCM&tags=broke");
-//        Object gifJson = target.findGif();
 
-        System.out.println();
 
     }
 
